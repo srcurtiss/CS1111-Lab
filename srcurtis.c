638be449a82f81560c91fdf28c1654a32e4d11d1
Shealyn Curtis <srcurtis@colossus.it.mtu.edu>
2024-08-30 15:40:11 -0400

    la la la

---
+++ srcurtis.c
@@ -0,0 +1,17 @@
+/*
+ * CS1111 Lab1
+ *
+ * @author Shea Curtis
+ * @email srcurtis@mtu.edu
+ * @date 8/30/2024
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, Shea! \n");
+	return 0;
+}
+
