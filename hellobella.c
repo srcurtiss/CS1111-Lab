be34b1457d6691a1e7ec4c905de415ca6dd32bc6
Isabella Hasaj <ihasaj@c28-0112-21.ad.mtu.edu>
2024-08-30 15:54:58 -0400

    first commit

---
+++ hellobella.c
@@ -0,0 +1,16 @@
+/* CS1111 Lab1
+ *
+ *@author Isabella Hasaj
+ *@ email ihasaj@mtu.edu
+ *@date 08-30-2024
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, Bella! \n");
+	return 0;
+}
+
