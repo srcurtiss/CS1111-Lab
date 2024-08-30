5aa04e983199b48fcbe7ba0b80d8267372a11f53
Derek Gehring <dtgehrin@c28-0112-22.ad.mtu.edu>
2024-08-30 15:57:25 -0400

    Helpful commit message.

---
+++ dtgehrin.c
@@ -0,0 +1,17 @@
+/*CS1111 Lab1
+ *
+ * @author Derek Gehring
+ * @Email dtgehring@mtu.edu
+ * @date 8/30/2024
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, Derek! \n");
+	return 0;
+}
+
+
