5fa1f678c521c46b95c6e8c30b473b933e0f64e6
srcurtiss <srcurtis@mtu.edu>
2024-09-13 15:48:02 -0400

    Update pressure.c

--- week3/pressure.c
+++ week3/pressure.c
@@ -9,7 +9,7 @@
  */
 
 #include <stdio.h>
-
+// Initialize varibles
 int main()
 {
 	double pressure;
@@ -17,7 +17,7 @@ int main()
 		printf("How deep is the lake (in meters)?");
 	scanf("%lf",&lakeDepth);
 
-
+// Create an if statement for the depth of the lake
 	if(lakeDepth<12)
 	{
 		printf("You can probably dive in it as a beginner.\n");
@@ -32,6 +32,7 @@ int main()
 		else
 		{ printf("You may want to make preparations before diving.\n");
 }
+// Calculate and display pressure for the depth		
 }
  pressure=(1000*9.8*lakeDepth)/1000;
  printf("The pressure at the deepest point is %.1lf kPa.\n", pressure);
