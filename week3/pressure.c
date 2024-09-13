c0a687ee688a59e1d9f64f61504e281eb8fcd381
Shealyn Curtis <srcurtis@colossus.it.mtu.edu>
2024-09-13 15:20:21 -0400

    Completed Q2

---
+++ week3/pressure.c
@@ -0,0 +1,39 @@
+/*
+ *
+ * CS1111 Lab Week 3
+ *
+ * @author Shea Curtis, Luke Johnson
+ * @email srcurtis@mtu.edu, lukej@mtu.edu
+ * @date 9/13/2024
+ *
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	double pressure;
+	double lakeDepth;
+		printf("How deep is the lake (in meters)?");
+	scanf("%lf",&lakeDepth);
+
+
+	if(lakeDepth<12)
+	{
+		printf("You can probably dive in it as a beginner.\n");
+
+	}
+	else
+	{
+		if(lakeDepth<30)
+	{
+		 printf("You can probably dive in it if you are a trained professional.\n");
+	}
+		else
+		{ printf("You may want to make preparations before diving.\n");
+}
+}
+ pressure=(1000*9.8*lakeDepth)/1000;
+ printf("The pressure at the deepest point is %.1lf kPa.\n", pressure);
+}
+
